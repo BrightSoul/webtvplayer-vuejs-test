df500c8e00dafce5d998e4e9432867abb3c76f71
Moreno Gentili <brightsoul@tiscali.it>
2020-06-16 19:54:33 +0200

    Rimosso Vue dallo scope globale

--- src/main.js
+++ src/main.js
@@ -1,10 +1,2 @@
 import Skin from './models/skin';
-
-//Embed the player
-
-const playerContainer = document.getElementById('player-container');
-const playerOptions = { skin: Skin, segment: 1.0 };
-const videoId = 1;
-/* eslint-disable no-undef */
-WebTvPlayer.withCredentials = false;
-new WebTvPlayer(playerContainer, videoId, playerOptions);
+global.WebTvPlayer.Skin = Skin; //Punto di contatto con il player
