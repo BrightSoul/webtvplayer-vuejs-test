f006bcc0a9048b2e426d2a2177f3cfebf530f223
Moreno Gentili <brightsoul@tiscali.it>
2020-06-16 18:21:14 +0200

    Semplificato l'oggetto di root

--- src/models/skin.js
+++ src/models/skin.js
@@ -3,20 +3,9 @@ import Vue from 'vue';
 import SkinOne from '../components/SkinOne/SkinOne.vue';
 
 //Custom skin
-export default function Skin() {
-  //Just expose the render and (optionally) the destroy methods
-  Object.defineProperty(this, 'render', {
-    writable: false,
-    configurable: false,
-    value: render,
-  });
-  Object.defineProperty(this, 'destroy', {
-    writable: false,
-    configurable: false,
-    value: destroy,
-  });
+class Skin {
 
-  async function render() {
+  async render() {
     const player = this; //this is a WebTvPlayer, documented here: https://api.civicam.it/v1/embed/#custom-skins
     //this might change in the future as it might be best to pass the player instance as an argument to the render method
     const skinElement = document.createElement('div');
@@ -27,7 +16,7 @@ export default function Skin() {
     });
   }
 
-  function destroy() {
+  destroy() {
     const player = this;
     console.log('destroy has been invoked');
     //Any cleanup here
@@ -35,3 +24,5 @@ export default function Skin() {
     //No need to explicitly unsubscribe events: the player object is going to be destroyed anyway.
   }
 }
+
+export default Skin;
